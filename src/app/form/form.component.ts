86644f1a23efe4e0ec0fe1c44872078a81a31028
Paulina Twarog <paulina.twarog@gmail.com>
2017-08-01 21:37:28 +0200

    Basic initialization for submitting a form.

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,13 +7,28 @@ import { FormGroup, FormBuilder } from '@angular/forms';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent implements OnInit {
+  public productForm: FormGroup;
 
-
-  constructor() { 
+  constructor(fb: FormBuilder) { 
+    this.productForm = fb.group({
+      name: [''],
+      surname: [''],
+      email: [''],
+      address: [''],
+      product: [''],
+      productscount: ['']
+    });
    }
 
   ngOnInit() {
   }
 
+  onSubmit(){
+    console.log("name: "+ this.productForm.value.name + 
+                " surname: " + this.productForm.value.surname + 
+                " email: " + this.productForm.value.email + 
+                " address: " + this.productForm.value.product + 
+                " productscount: " + this.productForm.value.productscount);
+  }
 
 }
